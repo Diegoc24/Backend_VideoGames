8b466f23ec03bc9c6e4af4bc9173eeafcbe171ec
Diegoc24 <carusodiegod@gmail.com>
2023-07-31 19:16:14 -0300

    feature/Backend(modificationBackend)

--- src/controllers/getVideoGame.js
+++ src/controllers/getVideoGame.js
@@ -4,7 +4,7 @@ const {Videogame, Genres } = require("../db")
 const getVideoGames = async ()=>{
     let apiGames;
     let game = []
-    for(let i = 2;i < 8; i++){
+    for(let i = 2;i < 4; i++){
        
             apiGames = (await axios.get(`https://api.rawg.io/api/games?key=cc0fe3294aa4466b8e8ff052a431c732&page=${i}`)).data.results    
             game = [...game, ...apiGames]   
